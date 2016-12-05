16e181ad86a6deb28afb6e977602b007f0510c3a
eSilvac <esilva1995@gmail.com>
2016-12-05 17:11:47 -0500

    Solucion Reto5 Rails

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,20 +2,20 @@
 #
 # Table name: rooms
 #
-#  id          :integer          not null, primary key
-#  title       :string
-#  description :text
-#  beds        :integer
-#  guests      :integer
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#  image_url   :string
+#  id              :integer          not null, primary key
+#  title           :string
+#  description     :text
+#  beds            :integer
+#  guests          :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  image_url       :string
+#  price_per_night :decimal(, )
 #
 
 class Room < ActiveRecord::Base
-	validates :title, presence: true
+	validates :title, :price_per_night, :image_url, presence: true
 	validates :description, presence: true, length: { maximum: 400 }
 	validates :beds, presence: true, numericality: { only_integer: true } 
 	validates :guests, presence: true, numericality: { only_integer: true } 
-	validates :image_url, presence: true
 end
